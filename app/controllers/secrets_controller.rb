4ca5c2d4b81c66d770aca6d35d6c2ee1ebd1d120
Scott Helms <shelms@imageoptions.net>
2017-02-13 15:18:06 -0800

    added secrets#create - tests pass

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,6 +1,11 @@
 class SecretsController < ApplicationController
   def create
     @secret = Secret.new(content: params[:new_secret], user: current_user)
+    if @secret.save
+      redirect_to current_user
+    else
+      flash[:errors] = @secret.errors.full_messages
+    end
   end
 
   def index
